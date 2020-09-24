a6857a7d9bd289edd085099c49dd6a6c480e4242
Darko Stefanovic <darkost0502@gmail.com>
2020-09-24 20:51:16 +0200

    Izbrisani komentari

--- PocetakGit/LoginTest.java
+++ PocetakGit/LoginTest.java
@@ -7,13 +7,13 @@ import org.testng.annotations.BeforeClass;
 
 public class LoginTest {
 
-//	@BeforeClass
-//	public void beforClass() {
-//		System.out.println("Before Class");
-//		System.setProperty("webdriver.chrome.driver", "driver-lib\\chromedriver.exe");
-//		this.driver = new ChromeDriver();
-//		JavascriptExecutor js = (JavascriptExecutor) driver;
-//		this.driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-//		this.driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-//}
+	@BeforeClass
+	public void beforClass() {
+		System.out.println("Before Class");
+		System.setProperty("webdriver.chrome.driver", "driver-lib\\chromedriver.exe");
+		this.driver = new ChromeDriver();
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		this.driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		this.driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+}
 }
